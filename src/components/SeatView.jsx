f0fd0dfa83d0a56a501699ea0bfca956162ed1a8
Aryan Singh <aryan.singh.lamba@gmail.com>
2023-11-23 16:47:47 -0800

    seat view connected to api

--- src/components/SeatView.jsx
+++ src/components/SeatView.jsx
@@ -1,57 +1,68 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { Button, Form, Modal } from "react-bootstrap"
 import { toast } from "react-toastify"
+import { useAddEntityMutation, useGetEntitiesByFieldQuery, useGetEntitiesQuery, useGetEntityQuery } from "../services/lastmeal"
 
 export const SeatView = ({ seat }) => {
-  const [teacher, setTeacher] = useState({
-    attributes: {
-      name: "John Doe",
-      teacher_status: "ARRIVED",
-      orders: [
-        { id: 1, attributes: { status: "UNFINISHED" } },
-        { id: 2, attributes: { status: "COOKED" } },
-        { id: 3, attributes: { status: "SERVED" } },
-      ]
-    }
-  })
+  console.log(seat)
+  const { data: teacher } = useGetEntityQuery({ name: "teacher", id: seat.attributes.teacher.data.id })
 
-  const allItems = [
-    { id: 1, attributes: { name: "Pancakes", type: "FOOD" } },
-    { id: 2, attributes: { name: "Burger", type: "FOOD" } },
-    { id: 3, attributes: { name: "Coke", type: "DRINK" } },
-    { id: 4, attributes: { name: "Sprite", type: "DRINK" } },
-    { id: 5, attributes: { name: "Water", type: "DRINK" } },
-    { id: 6, attributes: { name: "Coffee", type: "DRINK" } },
-  ]
+  const { data: allItems } = useGetEntitiesQuery({ name: "item", populate: true })
 
-  const [items, setItems] = useState([allItems[0]])
+  const { data: orders } = useGetEntitiesByFieldQuery({ name: "order", field: "teacher", value: seat.attributes.teacher.data.id, relation: 'id', populate: true })
+
+  const [items, setItems] = useState([])
+
+  const [addEntity] = useAddEntityMutation();
+
+  const [submitted, setSubmitted] = useState(false)
 
   const addItem = () => {
-    console.log([...items, allItems[0]])
-    setItems([...items, allItems[0]])
+    console.log([...items, allItems.data[0]])
+    setItems([...items, allItems.data[0]])
+  }
+
+  const removeItem = (id) => {
+    setItems(items.filter(item => item.id !== id))
   }
 
   const submitOrder = () => {
-    console.log(items)
-    setItems([allItems[0]])
+    const foodItems = items.filter(item => item.attributes.type === "FOOD")
+    const drinkItems = items.filter(item => item.attributes.type === "DRINK")
+    if (foodItems.length > 0) {
+      addEntity({ name: "order", body: { data: { status: "UNFINISHED", items: foodItems.map(item => item.id), teacher: seat.attributes.teacher.data.id, type: "KITCHEN" } } })
+    }
+    if (drinkItems.length > 0) {
+      addEntity({ name: "order", body: { data: { status: "UNFINISHED", items: drinkItems.map(item => item.id), teacher: seat.attributes.teacher.data.id, type: "BAR" } } })
+    }
+    setSubmitted(true)
     toast.success("Order submitted!")
   }
 
-  return <Modal.Body>
+  useEffect(() => {
+    if (allItems) {
+      setItems([allItems.data[0]])
+    }
+  }, [allItems])
+
+  return teacher && allItems && orders && <Modal.Body>
     <div>
-      <h4>{teacher.attributes.name}</h4>
-      <h5>Status: {teacher.attributes.teacher_status}</h5>
-      {teacher.attributes.teacher_status === "ARRIVED" && <>
-        <h5>{teacher.attributes.orders.filter(o => o.attributes.status !== "SERVED").length} orders in progress</h5>
-        <h5>{teacher.attributes.orders.filter(o => o.attributes.status === "SERVED").length} orders completed</h5>
-        <h5>Create New Order:</h5>
-        <ul>
-          {items.map(item => <Form.Select value={item.id} onChange={(e) => { setItems(items.map((i, index) => (index === items.indexOf(item) ? allItems[e.target.value - 1] : i))); console.log(items) }}>
-            {allItems.map(allItem => <option value={allItem.id}>{allItem.attributes.name}</option>)}
-          </Form.Select>)}
-          <Button onClick={addItem}>Add Item</Button>
-        </ul>
-        <Button onClick={submitOrder}>Submit Order</Button>
+      <h4>{teacher.data.attributes.name}</h4>
+      <h5>Status: {teacher.data.attributes.teacher_status}</h5>
+      {teacher.data.attributes.teacher_status === "ARRIVED" && <>
+        <h5>{orders.data.filter(o => o.attributes.status !== "SERVED").length} orders in progress</h5>
+        <h5>{orders.data.filter(o => o.attributes.status === "SERVED").length} orders completed</h5>
+        {!submitted && <> <h5>Create New Order:</h5>
+          <ul>
+            {items.map(item => <p><Form.Select value={item.id} onChange={(e) => { setItems(items.map((i, index) => (index === items.indexOf(item) ? allItems.data[e.target.value - 1] : i))); console.log(items) }}>
+              {allItems.data.map(allItem => <option value={allItem.id}>{allItem.attributes.name}</option>)}
+            </Form.Select>
+              <Button variant="danger" onClick={() => removeItem(item.id)}>Remove Item</Button></p>)}
+            <Button onClick={addItem}>Add Item</Button>
+          </ul>
+          <Button onClick={submitOrder}>Submit Order</Button>
+        </>}
+        {submitted && <h5>Order Submitted!</h5>}
       </>}
     </div>
   </Modal.Body>

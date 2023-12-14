0a3ffa1a0b39299957d932231e74e5814da54316
Aryan Singh <aryan.singh.lamba@gmail.com>
2023-12-13 17:52:09 -0800

    drinks

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -3,40 +3,44 @@ import { Button, Card, Modal } from 'react-bootstrap'
 import ItemView from './ItemView'
 import styles from './styles/ItemCard.module.css'
 
-const ItemCard = ({item, imageAdress, addItem, small}) => {
+const ItemCard = ({ item, imageAdress, addItem, small }) => {
+  console.log(item)
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => {
+    setShow(false)
+  };
+
+  const customize = () => {
     console.log(item)
+    if (item.attributes.type === "DRINK") {
+      addItem(item.attributes.name, "DRINK")
+      return
+    }
+    setShow(true)
+    console.log("TEST")
+    console.log(item)
+  };
 
-    const [show, setShow] = useState(false);
-    
-    const handleClose = () => {
-        setShow(false)
-    };
-
-    const handleShow = () => {
-        setShow(true)
-        console.log("TEST")
-        console.log(item)
-    };
-
-    return (
-        
-        <div>
-            {/*cards*/}
-            <Card style={small? {width: '7rem' }:{ width: '13rem' }}>
-                {imageAdress && <Card.Img className={styles.image} variant="top" src={imageAdress} />}
-                    <Card.Body>
-                        <Card.Title>{item.attributes.name}</Card.Title>
-                        <Button className={styles[`card-button`]}variant="info" onClick={handleShow}>Add Item</Button>
-                    </Card.Body>
-                </Card>
-            {/*modal*/}
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header className={styles[`modal-header`]} closeButton>
-                    <Modal.Title className={styles[`modal-heading`]}>{item.attributes.name}</Modal.Title>
-                </Modal.Header>
-                <ItemView item={item} addItem={addItem} closeSelf={handleClose}></ItemView>
-            </Modal>
-        </div>
+  return (
+    <div>
+      {/*cards*/}
+      <Card style={small ? { width: '7rem' } : { width: '13rem' }}>
+        {imageAdress && <Card.Img className={styles.image} variant="top" src={imageAdress} />}
+        <Card.Body>
+          <Card.Title>{item.attributes.name}</Card.Title>
+          <Button className={styles[`card-button`]} variant="info" onClick={customize}>Add Item</Button>
+        </Card.Body>
+      </Card>
+      {/*modal*/}
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header className={styles[`modal-header`]} closeButton>
+          <Modal.Title className={styles[`modal-heading`]}>{item.attributes.name}</Modal.Title>
+        </Modal.Header>
+        <ItemView item={item} addItem={addItem} closeSelf={handleClose}></ItemView>
+      </Modal>
+    </div>
   )
 }
 

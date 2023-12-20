9f3bfcbbd943a06b859d7b34d5a23477ceabbb85
Aryan Singh <aryan.singh.lamba@gmail.com>
2023-12-19 18:36:21 -0800

    allorders refetch every 10 seconds

--- src/components/AllOrders.jsx
+++ src/components/AllOrders.jsx
@@ -1,100 +1,110 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useGetNestedEntitiesQuery } from '../services/lastmeal';
 import styles from './styles/AllOrders.module.css'
 import './styles/AllOrders.css'
+import socket from '../sockets/socket';
 
-const AllOrders = ({type, tableId}) => {
-    const { data: orders } = useGetNestedEntitiesQuery({ name: "order", populate: true, fields:['teacher', 'seat', 'table']});
-    const currentDate = new Date()
+const AllOrders = ({ type, tableId }) => {
+  const { data: orders, refetch } = useGetNestedEntitiesQuery({ name: "order", populate: true, fields: ['teacher', 'seat', 'table'] });
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      console.log("refetch")
+      refetch();
+    }, 10000);
+
+    return () => clearInterval(interval);
+  }, [refetch])
+
   return (
-    <div className={styles[`ao-container`]}style={{display:"flex",flexDirection:"row", justifyContent:"center"}}>
-     { !tableId? 
-    <table class="orders"> 
-                <tr class='ao-heading'>
-                <th class='sticky'>
-                        <h5 className={styles[`table-heading`]}>Pickup at</h5>
-                    </th>
-                    <th class='sticky'>
-                        <h5 className={styles[`table-heading`]}>Teacher</h5>
-                    </th>
-                    <th class='sticky'>
-                        <h5 className={styles[`table-heading`]}>Table</h5>
-                    </th>
-                    <th class='sticky'>
-                        <h5 className={styles[`table-heading`]}>Seat</h5>
-                    </th>
-                    <th class='sticky'>
-                        <h5 className={styles[`table-heading`]}>Item</h5>
-                    </th>
-                </tr>
+    <div className={styles[`ao-container`]} style={{ display: "flex", flexDirection: "row", justifyContent: "center" }}>
+      {!tableId ?
+        <table class="orders">
+          <tr class='ao-heading'>
+            <th class='sticky'>
+              <h5 className={styles[`table-heading`]}>Pickup at</h5>
+            </th>
+            <th class='sticky'>
+              <h5 className={styles[`table-heading`]}>Teacher</h5>
+            </th>
+            <th class='sticky'>
+              <h5 className={styles[`table-heading`]}>Table</h5>
+            </th>
+            <th class='sticky'>
+              <h5 className={styles[`table-heading`]}>Seat</h5>
+            </th>
+            <th class='sticky'>
+              <h5 className={styles[`table-heading`]}>Item</h5>
+            </th>
+          </tr>
 
-        {orders && orders.data.filter(o => o.attributes.status === type).map(
-            o => 
-            <>{o.attributes.items.split(",").map(item => 
-            
+          {orders && orders.data.filter(o => o.attributes.status === type).map(
+            o =>
+              <>{o.attributes.items.split(",").map(item =>
+
                 <tr class="ao-row">
-                    <td class={`ao-item-${type} first`}>
-                        {o.attributes.type}
-                    </td>
-                    <td class={`ao-item-${type}`}>
-                        {o.attributes.teacher.data.attributes.name}   
-                    </td>
-                    <td class={`ao-item-${type}`}>
-                        {o.attributes.teacher.data.attributes.seat.data ? o.attributes.teacher.data.attributes.seat.data.attributes.table.data.attributes.number : ''}
-                    </td>
-                    <td class={`ao-item-${type}`}>
-                        {o.attributes.teacher.data.attributes.seat.data ? o.attributes.teacher.data.attributes.seat.data.attributes.number : ''}
-                    </td>
-                    <td class={`ao-item-${type} last`}>
+                  <td class={`ao-item-${type} first`}>
+                    {o.attributes.type}
+                  </td>
+                  <td class={`ao-item-${type}`}>
+                    {o.attributes.teacher.data.attributes.name}
+                  </td>
+                  <td class={`ao-item-${type}`}>
+                    {o.attributes.teacher.data.attributes.seat.data ? o.attributes.teacher.data.attributes.seat.data.attributes.table.data.attributes.number : ''}
+                  </td>
+                  <td class={`ao-item-${type}`}>
+                    {o.attributes.teacher.data.attributes.seat.data ? o.attributes.teacher.data.attributes.seat.data.attributes.number : ''}
+                  </td>
+                  <td class={`ao-item-${type} last`}>
                     {item}
-                    </td>
+                  </td>
                 </tr>
-           )}
-        </>)}
+              )}
+              </>)}
         </table>
-        : 
+        :
         <div>
-        <h3>Prepared Items</h3>
-        <table class="orders"> 
-                <tr class='ao-heading'>
-                <th class='sticky'>
-                        <h5 className={styles[`table-heading`]}>Pickup at</h5>
-                    </th>
-                    <th class='sticky'>
-                        <h5 className={styles[`table-heading`]}>Teacher</h5>
-                    </th>
-                    <th class='sticky'>
-                        <h5 className={styles[`table-heading`]}>Seat</h5>
-                    </th>
-                    <th class='sticky'>
-                        <h5 className={styles[`table-heading`]}>Item</h5>
-                    </th>
-                </tr>
+          <h3>Prepared Items</h3>
+          <table class="orders">
+            <tr class='ao-heading'>
+              <th class='sticky'>
+                <h5 className={styles[`table-heading`]}>Pickup at</h5>
+              </th>
+              <th class='sticky'>
+                <h5 className={styles[`table-heading`]}>Teacher</h5>
+              </th>
+              <th class='sticky'>
+                <h5 className={styles[`table-heading`]}>Seat</h5>
+              </th>
+              <th class='sticky'>
+                <h5 className={styles[`table-heading`]}>Item</h5>
+              </th>
+            </tr>
 
-        {orders && orders.data.filter(o => o.attributes.status === type && o.attributes.teacher.data.attributes.seat.data.attributes.table.data.id === tableId).map(
-            o => 
-            <>{o.attributes.items.split(",").map(item => 
-            
-                <tr class="ao-row">
+            {orders && orders.data.filter(o => o.attributes.status === type && o.attributes.teacher.data.attributes.seat.data.attributes.table.data.id === tableId).map(
+              o =>
+                <>{o.attributes.items.split(",").map(item =>
+
+                  <tr class="ao-row">
                     <td class={`ao-item-${type} first`}>
-                        {o.attributes.type}
+                      {o.attributes.type}
                     </td>
                     <td class={`ao-item-${type}`}>
-                        {o.attributes.teacher.data.attributes.name}
-                        
+                      {o.attributes.teacher.data.attributes.name}
+
                     </td>
                     <td class={`ao-item-${type}`}>
-                        {o.attributes.teacher.data.attributes.seat.data.attributes.number}
+                      {o.attributes.teacher.data.attributes.seat.data.attributes.number}
                     </td>
                     <td class={`ao-item-${type} last`}>
-                    {item}
+                      {item}
                     </td>
-                </tr>
-           )}
-        </>)}
-        </table>
+                  </tr>
+                )}
+                </>)}
+          </table>
         </div>
-        }
+      }
     </div>
   )
 }
--- src/services/lastmeal.js
+++ src/services/lastmeal.js
@@ -18,7 +18,7 @@ export const lastmealApi = createApi({
     getEntity: builder.query({
       query: (arg) => {
         const { name, id, populate } = arg;
-        return `${pluralize(name.replace('_', '-'))}/${id}`+ (populate ? '?populate=*' : '')
+        return `${pluralize(name.replace('_', '-'))}/${id}` + (populate ? '?populate=*' : '')
       }
     }),
     getEntities: builder.query({
@@ -32,7 +32,7 @@ export const lastmealApi = createApi({
     }),
     getEntitiesByDepth: builder.query({
       query: (arg) => {
-        const { name, populate, depthField} = arg;
+        const { name, populate, depthField } = arg;
         let query = `${pluralize(name.replace('_', '-'))}`
         if (populate) query += '?populate[0]=' + depthField;
         console.log("query");
@@ -42,7 +42,7 @@ export const lastmealApi = createApi({
     }),
     getEntitiesByDepth2: builder.query({
       query: (arg) => {
-        const { name, populate, depthField1, depthField2} = arg;
+        const { name, populate, depthField1, depthField2 } = arg;
         let query = `${pluralize(name.replace('_', '-'))}`
         if (populate) query += '?populate[' + depthField1 + '][populate][0]=' + depthField2;
         console.log("query");
@@ -52,14 +52,14 @@ export const lastmealApi = createApi({
     }),
     getNestedEntities: builder.query({
       query: (arg) => {
-        const { name, populate, fields} = arg;
+        const { name, populate, fields } = arg;
         let query = `${pluralize(name.replace('_', '-'))}`
-        if (populate){
+        if (populate) {
           query += '?populate='
           for (const field of fields) {
-            query+=  field + '.';
+            query += field + '.';
+          }
         }
-      }
         console.log("nested query");
         console.log(query);
         return query
@@ -192,131 +192,6 @@ export const lastmealApi = createApi({
           method: 'GET',
         }
       },
-    }),
-    getMessages: builder.query({
-      query: (gameroom) => `messages?filters[gameroom][id]=${gameroom}&populate=*&pagination[pageSize]=1000`,
-      async onCacheEntryAdded(
-        arg,
-        { updateCachedData, cacheDataLoaded, cacheEntryRemoved, dispatch }
-      ) {
-        // create a websocket connection when the cache subscription starts        
-        const socket = io(process.env.REACT_APP_API_HOST);
-        try {
-          socket.on('connect', () => {
-            console.log("connected to socket")
-            //join the game room
-            socket.emit("join", { gameroom: arg }, (error) => {
-              console.log("joined the room:" + arg)
-              if (error) return alert(error);
-            });
-          });
-
-          // wait for the initial query to resolve before proceeding
-          await cacheDataLoaded
-          // when data is received from the socket connection to the server,
-          // if it is a message and for the appropriate channel,
-          // update our query result with the received message
-          socket.on('message', (message) => {
-            updateCachedData((draft) => {
-              draft.data.push(message);
-              dispatch({
-                type: `govsimApi/invalidateTags`,
-                payload: [{ type: 'promise', id: 'LIST' }],
-              });
-            });
-          });
-
-          socket.on('new_party', (message) => {
-            toast('A new party has joined the Game : ' + message.name)
-            dispatch({
-              type: `govsimApi/invalidateTags`,
-              payload: [{ type: 'party', id: 'LIST' }],
-            });
-          });
-
-
-          socket.on('challenge_updated', (message) => {
-            const user = JSON.parse(localStorage.getItem('user')).user
-            if (message.to_player.id === user.id || message.from_player.id === user.id) {
-              dispatch(showAlert({
-                show: false
-              }));
-              //if the status is Accepted, then redirect to the game
-              if (message.status === 'ACCEPTED') {
-                window.location = '/game/' + message.game.url + '/' + message.id;
-              }
-
-            }
-          });
-
-          socket.on('challenge', (message) => {
-            const user = JSON.parse(localStorage.getItem('user')).user
-            if (message.to_player.id === user.id) {
-              dispatch(showAlert({
-                show: true,
-                title: 'Challenge',
-                showSpinner: true,
-                message: 'You have been challenged by ' + message.from_player.name + ' for a game of ' + message.game.name,
-                msgBody: message,
-                showConfirmButton: true,
-                allowOutsideClick: false,
-                allowEscapeKey: false,
-                showDenyButton: true,
-                showCancelButton: false,
-                confirmButtonText: 'Accept',
-                denyButtonText: `Reject`,
-              }));
-            }
-            if (message.from_player.id === user.id) {
-              dispatch(showAlert({
-                show: true,
-                title: 'Challenge',
-                showSpinner: true,
-                message: 'Your challenge has been sent to ' + message.to_player.name + ' for a game of ' + message.game.name,
-                msgBody: message,
-                timer: 60000,
-                timerProgressBar: true,
-                showConfirmButton: false,
-                allowOutsideClick: false,
-                allowEscapeKey: false,
-                showCancelButton: true
-              }));
-            }
-          });
-
-          socket.on('election_finished', (message) => {
-            dispatch(showAlert({
-              show: false,
-            }));
-
-            setTimeout(() => {
-              dispatch(showAlert({
-                show: true,
-                showSpinner: false,
-                title: 'Elections',
-                message: 'Elections result are out',
-              }));
-            }, 100);
-
-
-            dispatch({
-              type: `govsimApi/invalidateTags`,
-              payload: [{ type: 'country', id: 'LIST' }, { type: 'party', id: 'LIST' }],
-            });
-          });
-
-
-        } catch {
-          // no-op in case `cacheEntryRemoved` resolves before `cacheDataLoaded`,
-          // in which case `cacheDataLoaded` will throw
-        }
-        // cacheEntryRemoved will resolve when the cache subscription is no longer active
-        await cacheEntryRemoved
-        // perform cleanup steps once the `cacheEntryRemoved` promise resolves
-        socket.off('connect');
-        socket.off('message');
-      },
-      providesTags: (result, error, arg) => [{ type: 'message', id: 'LIST' }],
     })
   })
 })
@@ -338,6 +213,5 @@ export const {
   useUpdateEntityMutation,
   useUpdateMessagesReadMutation,
   useCreateSessionMutation,
-  useGetSessionMutation,
-  useGetMessagesQuery
+  useGetSessionMutation
 } = lastmealApi

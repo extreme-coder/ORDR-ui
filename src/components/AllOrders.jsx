817e84bb4017ee0a49ec0107a867cc12415b5d5a
ahosseini06 <ali20060101@gmail.com>
2023-12-20 11:21:44 -0800

    bug fix

--- src/components/AllOrders.jsx
+++ src/components/AllOrders.jsx
@@ -67,43 +67,42 @@ const AllOrders = ({ type, tableId }) => {
         <div>
           <h3>Prepared Items</h3>
           <table class="orders">
-            <tr class='ao-heading'>
-              <th class='sticky'>
-                <h5 className={styles[`table-heading`]}>Pickup at</h5>
-              </th>
-              <th class='sticky'>
-                <h5 className={styles[`table-heading`]}>Teacher</h5>
-              </th>
-              <th class='sticky'>
-                <h5 className={styles[`table-heading`]}>Seat</h5>
-              </th>
-              <th class='sticky'>
-                <h5 className={styles[`table-heading`]}>Item</h5>
-              </th>
-            </tr>
+          <tr class='ao-heading'>
+            <th class='sticky'>
+              <h5 className={styles[`table-heading`]}>Pickup at</h5>
+            </th>
+            <th class='sticky'>
+              <h5 className={styles[`table-heading`]}>Teacher</h5>
+            </th>
+            <th class='sticky'>
+              <h5 className={styles[`table-heading`]}>Seat</h5>
+            </th>
+            <th class='sticky'>
+              <h5 className={styles[`table-heading`]}>Item</h5>
+            </th>
+          </tr>
 
-            {orders && orders.data.filter(o => o.attributes.status === type && o.attributes.teacher.data.attributes.seat.data.attributes.table.data.id === tableId).map(
-              o =>
-                <>{o.attributes.items.split(",").map(item =>
+          {orders && orders.data.filter(o => o.attributes.status === type).map(
+            o =>
+              <>{o.attributes.items.split(",").map(item =>
 
-                  <tr class="ao-row">
-                    <td class={`ao-item-${type} first`}>
-                      {o.attributes.type}
-                    </td>
-                    <td class={`ao-item-${type}`}>
-                      {o.attributes.teacher.data.attributes.name}
-
-                    </td>
-                    <td class={`ao-item-${type}`}>
-                      {o.attributes.teacher.data.attributes.seat.data.attributes.number}
-                    </td>
-                    <td class={`ao-item-${type} last`}>
-                      {item}
-                    </td>
-                  </tr>
-                )}
-                </>)}
-          </table>
+                <tr class="ao-row">
+                  <td class={`ao-item-${type} first`}>
+                    {o.attributes.type}
+                  </td>
+                  <td class={`ao-item-${type}`}>
+                    {o.attributes.teacher.data.attributes.name}
+                  </td>
+                  <td class={`ao-item-${type}`}>
+                    {o.attributes.teacher.data.attributes.seat.data ? o.attributes.teacher.data.attributes.seat.data.attributes.number : ''}
+                  </td>
+                  <td class={`ao-item-${type} last`}>
+                    {item}
+                  </td>
+                </tr>
+              )}
+              </>)}
+        </table>
         </div>
       }
     </div>
